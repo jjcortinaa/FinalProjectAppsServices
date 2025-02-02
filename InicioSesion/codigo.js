9c98d3550d2686553c6dae3c94fcff9a446172a1
Jose Juan <>
2025-02-02 12:33:07 +0100

    html register and sign in

---
+++ InicioSesion/codigo.js
@@ -0,0 +1,9 @@
+
+function validate_password() {
+    let contrasena = document.getElementById("password")
+    let contrasena_confirm = document.getElementById("password_confirm")
+    if (contrasena===contrasena_confirm){
+        
+    }
+
+}
