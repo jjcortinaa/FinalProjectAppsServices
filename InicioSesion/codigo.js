24f10055d3b9c63769de66bc3a09fe572c06e615
XaierAlizu <202207372@alu.comillas.edu>
2025-02-03 14:41:01 +0100

    Password validation

--- InicioSesion/codigo.js
+++
@@ -1,9 +0,0 @@
-
-function validate_password() {
-    let contrasena = document.getElementById("password")
-    let contrasena_confirm = document.getElementById("password_confirm")
-    if (contrasena===contrasena_confirm){
-        
-    }
-
-}
