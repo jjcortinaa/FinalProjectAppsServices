74518e6442ed594ee820f91c4277e61a0774e358
XabierAlbizu <202207372@alu.comillas.edu>
2025-02-09 08:39:31 +0100

    PT1-Home_InicioSesion_Registro

---
+++ src/home/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("search-btn").addEventListener("click", function() {
+        window.location.href = "resultado.html";  
+    });
+});
